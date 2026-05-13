62ededf32fd25c81ee2c1ffd71b4c9be98f00f5c
ZSzero <960227941@qq.com>
2026-05-13 18:34:51 +0800

    打印的buffer_out以及yunet_decode的输出貌似正确，可以试试画图了。他妈之前忘记烧模型了

--- ElectronicWarehouse/Appli/X-CUBE-AI/App/network.c
+++ ElectronicWarehouse/Appli/X-CUBE-AI/App/network.c
@@ -39,7 +39,7 @@
  * --Ocache-opt = true
  * --output-info-file = "c_info"
  * --onnx-input = "C:/Users/96022/.stm32cubemx/network_output/yunetn_320_qdq_int8_OE_3_3_0.onnx"
- * --out-dir-prefix = "C:/Users/96022/AppData/Local/Temp/mxAI_workspace2389009233050011232440456191662616/neural_art__network/"
+ * --out-dir-prefix = "C:/Users/96022/AppData/Local/Temp/mxAI_workspace13357215326009893011130542470266/neural_art__network/"
  * --all-buffers-info = true
  * --mvei = true
  * --Oauto-sched = true
